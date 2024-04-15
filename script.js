c038dec3d734a2caf373f3f99b1c59a691a4084a
Aco Vreto <acovretoski@gmail.com>
2024-04-15 06:20:55 +0200

    zavrsena kosnicka

--- script.js
+++ script.js
@@ -32,51 +32,114 @@
 //     localStorage.setItem("parfumeName", parfumeName);
 //   });
 // });
-let subtotal=(price,kolicina=1)=>{
-return price*kolicina
-}
+
 let kupiSegaElements = document.querySelectorAll(".card-action-btn");
-let productInfoEl= document.querySelector(".offcanvas .product-info");
+
+let products = document.querySelector(".offcanvas .products");
 kupiSegaElements.forEach((el) => {
   el.addEventListener("click", function (e) {
     e.preventDefault();
-    console.log("zdravo");
+    let productsCount = products.querySelectorAll(
+      ".product-info .empty"
+    ).length;
+    console.log(productsCount);
+    if (productsCount == 1) {
+      products.innerHTML = "";
+    }
     let img = el.closest(".product-card").querySelector("img").src;
-    console.log(img)
-    let price = el.closest(".product-card").querySelector(".card-price").textContent;
-    console.log(price)
-    let title = el.closest(".product-card").querySelector("h3.card-title").textContent;
-    console.log(title);
+    let productTitle = el
+      .closest(".product-card")
+      .querySelector(".card-title a").textContent;
 
+    console.log(img);
+    let price = el.closest(".product-card").querySelector(".card-price").value;
+    console.log(price);
+    let title = el
+      .closest(".product-card")
+      .querySelector("h3.card-title a").textContent;
+    console.log(title);
+    let st = subtotal(Number(price));
+    console.log(st);
     let html = ` <div class="product-info">
     <div class="row product d-flex align-items-center justify-content-center">
-     <div class="col-4"><img class="cart-img" src="${img}" width="50" height="50" alt=""><span class="product-title">Product</span></div>
+     <div class="col-4"><img class="cart-img" src="${img}" width="50" height="50" alt=""><span class="product-title">${productTitle}</span></div>
      <div class="col-2"><span class="price">${price}</span>$</div>
      <div class="col-2"><input type="number" name="" id="kolicina" value="1"></div>
-     <div class="col-2 subtotal"><span class="subtotal">${subtotal(price)}</span><span>$</span></div>
-     <div class="col-2"><button class="btn btn-danger">
+     <div class="col-2"><span class="subtotal">${st}</span>$</div>
+     <div class="col-2"><button class="btn btn-danger trash">
        <i class="fa fa-trash-o"></i>
      </button></div>
     </div>
     
-    <div class="row total">
-      <div class="col-2">Тотал</div>
-     <div class="col-4"></div>
-     <div class="col-2"></div>
-     <div class="col-2 "><span class="total">200</span>$</div>
-     <div class="col-2"></div>
-
-    </div>
+   
    </div>`;
-    productInfoEl.insertAdjacentHTML("beforeend");
-    let subtotal=inputEl.addEventListener("change",subtotal.bind(this.value,price))
-    subtotalEl=document.querySelector(".subtotal")
-    subtotalEl.textContent=subtotal;
-    
+
+    products.insertAdjacentHTML("afterbegin", html);
+    let productInfoEl = document.querySelector(".offcanvas .product-info");
+    let allSubTotals = document
+      .querySelector(".offcanvas")
+      .querySelectorAll(".subtotal");
+    let total = 0;
+    allSubTotals.forEach((subtotal) => {
+      total += Number(subtotal.textContent);
+    });
+    totalEl = document.querySelector(".offcanvas").querySelector(".total");
+    totalEl.textContent = total;
+    const inputEls = products.querySelectorAll("input");
+    console.log(inputEls);
+    inputEls.forEach((input) => {
+      input.addEventListener("change", () => {
+        let priceValue = input
+          .closest(".product-info")
+          .querySelector(".price").textContent;
+        console.log(priceValue);
+
+        let subtotalEl = input
+          .closest(".product-info")
+          .querySelector(".subtotal");
+        subtotalEl.textContent = subtotal(
+          Number(priceValue),
+          Number(input.value)
+        );
+        let allSubTotals = document
+          .querySelector(".offcanvas")
+          .querySelectorAll(".subtotal");
+        let total = 0;
+        allSubTotals.forEach((subtotal) => {
+          total += Number(subtotal.textContent);
+        });
+        totalEl = document.querySelector(".offcanvas").querySelector(".total");
+        totalEl.textContent = total;
+      });
+    });
+    const trashEls = products.querySelectorAll(".trash");
+    trashEls.forEach((trash) => {
+      trash.addEventListener("click", () => {
+        if (products.querySelectorAll(".product-info").length == 1) {
+          trash.closest(".product-info").innerHTML =
+            "<h3 class='empty'>Кошничката е празна!</h3>";
+          totalEl.textContent = "0";
+        } else {
+          let closestProductIfo = trash.closest(".product-info");
+          console.log(closestProductIfo);
+          closestProductIfo.remove();
+          allSubTotals = document
+            .querySelector(".offcanvas")
+            .querySelectorAll(".subtotal");
+          let total = 0;
+          allSubTotals.forEach((subtotal) => {
+            total += Number(subtotal.textContent);
+          });
+          totalEl = document
+            .querySelector(".offcanvas")
+            .querySelector(".total");
+          totalEl.textContent = total;
+        }
+      });
+    });
   });
 });
-const checkoutEl = document.querySelector(".checkout");
-checkoutEl.addEventListener("click", (e) => {
-  e.preventDefault();
-  tableBody.innerHTML = "<h3>Кошницата е празна!</h3>";
-});
+let subtotal = (price, kolicina = 1) => {
+  console.log(price * kolicina);
+  return price * kolicina;
+};
